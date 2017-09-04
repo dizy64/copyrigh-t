9ac8bfc6049e4c63b185c2a5e5189d0dcefa2ce4
DongGeun Yee <i@dizy.me>
2017-09-04 17:44:06 +0900

    간단한 기능 구현 작업
    * 프론트 작업
    * 컨트롤러 작업
    
    ISSUE: #1

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,28 @@
 class ReportsController < ApplicationController
   def index
+    @report = Report.new
+    @total_count = Report.all.count
   end
 
   def new
   end
 
   def create
+
+    @report = Report.new(report_params)
+
+    if @report.save
+      flash[:success] = '정상적으로 신고되었습니다.'
+      redirect_to root_path
+    else
+      redirect_to status: 500
+    end
+
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:url, :description)
   end
 end
