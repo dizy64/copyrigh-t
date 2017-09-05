94807729bd24736860eff1653fcdf3f1fb6955f4
DongGeun Yee <i@dizy.me>
2017-09-06 01:52:46 +0900

    URL 통계 내기 기능 일부 구현.
    구현 내용 : &가 붙는 파라미터 이전 까지의 주소를 비교해서 동일한 URL인지 확인해본다.
    파라미터 순서가 다른건 당장은 어쩔수 없어서 놔두고 패턴을 분석해서 더 정교하게 나눌 방법을 찾아보도록 노력해야할듯.
    
    ISSUE: #8

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,17 @@
 class Report < ApplicationRecord
   validates :url, presence: true, url: true
+
+  scope :today, -> { where 'created_at >= ?', Time.zone.now.beginning_of_day }
+  scope :yesterday, -> {
+    where 'created_at >= ? and ? > created_at', Time.zone.now.beginning_of_day - 1.day, Time.zone.now.beginning_of_day
+  }
+
+  def self.arrange
+    result = []
+    self.pluck(:url).each do |u|
+      result << { url: u, count: self.where("url LIKE '#{ u.split('&').first }%'").count }
+    end
+    result
+  end
+  
 end
